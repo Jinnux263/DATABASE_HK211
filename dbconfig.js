480cb1d5372a865c1eb60f58133598a0df19b4fd
Jinnux <nguyentandat26032001@gmail.com>
2021-11-26 01:00:36 +0700

    database connected

---
+++ dbconfig.js
@@ -0,0 +1,12 @@
+const config = {
+    login: 'admin',
+    password: '123456',
+    database: 'ASSIGNMENT2',
+    server: 'MSI',
+    driver: 'msnodesqlv8',
+    options: {
+        trustedConnection: true
+    }
+    
+}
+module.exports = config; 
--- server.js
+++ server.js
@@ -1,10 +1,52 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function(req, res) {
-    res.send("Express here!")
-})
+const sql = require("mssql/msnodesqlv8");
+const config = new sql.ConnectionPool({
+    login: 'admin',
+    password: '123456',
+    database: 'ASSIGNMENT2',
+    server: 'MSI',
+    driver: 'msnodesqlv8',
+    options: {
+        trustedConnection: true
+    }
+});
+
+app.get('/', function (req, res) {
+    config.connect().then(() => {
+        config.request().query('select * from USERTB', (err, result) => {
+            res.send(result);
+        })
+        
+    }).catch(function(err) {
+        console.log(err)
+    });
+});
+
 
 app.listen(3001, function() {
     console. log("express server is running on port 3001");
-})
+});
+
+
+
+
+// var sql = require('mssql/msnodesqlv8');
+// var config = {
+//     connectionString: 'Driver=SQL Server;Server=localhost\\MSI;Database=master;Trusted_Connection=true;'
+// };
+// sql.connect(config, err => {
+//     new sql.Request().query('SELECT 1 AS justAnumber', (err, result) => {
+//         console.log(".:The Good Place:.");
+//         if(err) { // SQL error, but connection OK.
+//             console.log("  Shirtballs: "+ err);
+//         } else { // All is rosey in your garden.
+//             console.dir(result);
+//         };
+//     });
+// });
+// sql.on('error', err => { // Connection borked.
+// console.log(".:The Bad Place:.");
+// console.log("  Fork: "+ err);
+// });
