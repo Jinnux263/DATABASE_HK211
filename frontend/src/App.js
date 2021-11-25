20f60138abb06e2fbc318415b9dfc415fda13e9e
Jinnux <nguyentandat26032001@gmail.com>
2021-11-25 18:55:43 +0700

    ok

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
+import Header from './Component/Header';
+import Footer from './Component/Footer';
+
 function App() {
   return (
     <>
-      <h1>React here!</h1>
+      <Header/>
+      <main>
+        <h1>React here!</h1>
+      </main>
+      <Footer/>
     </>
   );
 }
