6b29fd18be40a7c6f2e04aaac5cf54e2b01b5f4d
= <=>
2021-11-26 15:41:16 +0700

    hmmm

--- dbconfig.js
+++ dbconfig.js
@@ -1,12 +1,12 @@
 const config = {
-    login: 'admin',
-    password: '123456',
-    database: 'ASSIGNMENT2',
-    server: 'MSI',
-    driver: 'msnodesqlv8',
-    options: {
-        trustedConnection: true
-    }
-    
-}
-module.exports = config; 
+  user: "LAPTOP-2KEU49GH\\Nguyen Dat",
+  password: "",
+  database: "ASSIGNMENT2",
+  server: "LAPTOP-2KEU49GH",
+  driver: "msnodesqlv8",
+  options: {
+    trustedConnection: true,
+  },
+  port: 61440,
+};
+module.exports = config;
